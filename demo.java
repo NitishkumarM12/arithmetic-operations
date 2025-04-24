61686ff0e8284678f414b87aa205d9f635cf4f12
NitishkumarM12 <mudaraddinitishkumar@outlook.com>
2025-04-24 11:09:36 +0530

    save file

--- demo.java
+++ demo.java
@@ -1,9 +1,13 @@
 public class demo{
     public static void main(String[] args) {
-        int a = 20;
-        int b = 10;
+        int a = 50;
+        int b = 100;
+        int add = a + b;
+        int sub = a - b;
         int multi = a * b;
         int division = a/b;
+        System.out.println("Addition :"+add);
+        System.out.println("Subtraction :"+sub);
         System.out.println("Multiplication:"+multi);
         System.out.println("Division :"+division);
     }
