349450e4b3b6d6e44676dd86d828c9bd5c01cdf8
NitishkumarM12 <mudaraddinitishkumar@outlook.com>
2025-04-24 10:54:12 +0530

    save file

---
+++ demo.java
@@ -0,0 +1,10 @@
+public class demo{
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int add = a + b;
+        int sub= a-b;
+        System.out.println("Addition:"+add);
+        System.out.println("Substraction :"+sub);
+    }
+}
